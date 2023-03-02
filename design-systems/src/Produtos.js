30c788664a767289363936b5e0df8800ecbd2c83
VieiraMiguel <peixotovieira.miguel@gmail.com>
2023-03-02 15:17:35 -0300

    Design Systems

---
+++ design-systems/src/Produtos.js
@@ -0,0 +1,47 @@
+export const Produtos = [
+    {
+        id: 1,
+        img:`https://picsum.photos/seed/1/400/400`,
+        marca: "Marca",
+        nome: "Este Produto",
+        cor: "Uma Cor",
+        preco1: 199,
+        preco2: 57
+    },
+    {
+        id: 2,
+        img:`https://picsum.photos/seed/2/400/400`,
+        marca: "Artesanal",
+        nome: "Coisa",
+        cor: "Color",
+        preco1: 150,
+        preco2: 120
+    },
+    {
+        id: 3,
+        img:`https://picsum.photos/seed/3/400/400`,
+        marca: "Brand",
+        nome: "Outra Coisa",
+        cor: "Coloração",
+        preco1: 70,
+        preco2: 55
+    },
+    {
+        id: 4,
+        img:`https://picsum.photos/seed/4/400/400`,
+        marca: "Importado",
+        nome: "Este Outro Produto",
+        cor: "Outra Cor",
+        preco1: 370,
+        preco2: 290
+    },
+    {
+        id: 5,
+        img:`https://picsum.photos/seed/5/400/400`,
+        marca: "Marca",
+        nome: "Produto",
+        cor: "Cor",
+        preco1: 230,
+        preco2: 150
+    }
+]
